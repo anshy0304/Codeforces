b06312fe655e10a22869744433bfbeb65455b2a6
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2025-10-16 00:55:48 +0530

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.java
@@ -0,0 +1,1 @@
+import java.util.*;public class Main{    public static void main(String[] args){        Scanner sc = new Scanner(System.in);        int t = sc.nextInt();        while(t-->0){            int x = sc.nextInt();            int s = sc.nextInt();            if(s%2 == 0) System.out.println(0);            else System.out.println(x);        }    }}
